addc3b8d038129b62e3100704b6568c11f02deec
emanel <manel.elhadi@ynov.com>
2024-03-07 15:09:28 +0100

    mise a jour de l'affichage des artistes ,Ajout de la fonction showArtistDetails

--- data/data.go
+++ data/data.go
@@ -1,15 +1,20 @@
-package data
+package main
 
 import (
 	"encoding/json"
-	"errors"
+	"fmt"
+	"image"
 	"net/http"
-	"strings"
+	"os"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
-var ErrArtistNotFound = errors.New("artist not found")
-var Art *Artist
-
+// Artist représente la structure des données récupérées de l'API
 type Artist struct {
 	ID           int      `json:"id"`
 	Image        string   `json:"image"`
@@ -18,78 +23,85 @@ type Artist struct {
 	CreationDate int      `json:"creationDate"`
 	FirstAlbum   string   `json:"firstAlbum"`
 	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concert_dates"`
-	Relation     string   `json:"relations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
-func GetID() int {
-	return Art.ID
-}
-
-func GetImage() string {
-	if Art == nil {
-		return "Image not available"
-	}
-	return Art.Image
-}
-
-func GetName() string {
-	return Art.Name
-}
-
-func GetMembers() []string {
-	return Art.Members
-}
-
-func GetCreationDate() int {
-	return Art.CreationDate
-}
-
-func GetFirstAlbum() string {
-	return Art.FirstAlbum
-}
-
-func GetLocations() string {
-	return Art.Locations
-}
-
-func GetConcertDates() string {
-	return Art.ConcertDates
-}
-
-func GetRelation() string {
-	return Art.Relation
-}
-
-func GetArtistByName(artistName string) (*Artist, error) {
-	const myurl = "https://groupietrackers.herokuapp.com/api/artists"
-	response, err := http.Get(myurl)
+// fetchArtists récupère les données de l'API
+func fetchArtists() ([]Artist, error) {
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
 		return nil, err
 	}
-	defer response.Body.Close()
+	defer resp.Body.Close()
 
 	var artists []Artist
-	err = json.NewDecoder(response.Body).Decode(&artists)
-	if err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(&artists); err != nil {
 		return nil, err
 	}
 
-	input := strings.ToLower(artistName)
+	return artists, nil
+}
 
-	var artist *Artist
-	for _, a := range artists {
-		if strings.ToLower(a.Name) == input {
-			artist = &a
-			break
-		}
+// fetchImage récupère une image depuis une URL et retourne un canvas.Image
+func fetchImage(url string) *canvas.Image {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Failed to load image:", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	img, _, err := image.Decode(resp.Body)
+	if err != nil {
+		fmt.Println("Failed to decode image:", err)
+		return nil
 	}
 
-	if artist == nil {
-		return nil, ErrArtistNotFound
+	return canvas.NewImageFromImage(img)
+}
+
+// showArtistDetails ouvre une nouvelle fenêtre avec les détails de l'artiste
+func showArtistDetails(a fyne.App, artist Artist) {
+	w := a.NewWindow(artist.Name)
+	info := fmt.Sprintf("Name: %s\nMembers: %v\nFirst Album: %s\nCreation Date: %d", artist.Name, artist.Members, artist.FirstAlbum, artist.CreationDate)
+	w.SetContent(widget.NewLabel(info))
+	w.Show()
+}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Groupie Tracker")
+
+	artists, err := fetchArtists()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erreur lors de la récupération des artistes: %v\n", err)
+		return
 	}
 
-	Art = artist
+	content := container.NewVBox()
 
-	return artist, nil
+	for _, artist := range artists {
+		artistInfo := fmt.Sprintf("Name: %s\nFirst Album: %s", artist.Name, artist.FirstAlbum)
+		artistLabel := widget.NewLabel(artistInfo)
+
+		img := fetchImage(artist.Image)
+		img.FillMode = canvas.ImageFillContain
+		img.SetMinSize(fyne.NewSize(100, 100))
+
+		infoButton := widget.NewButton("", func() {
+			showArtistDetails(a, artist)
+		})
+		infoButton.Importance = widget.LowImportance // Rend le bouton transparent
+		infoButton.SetIcon(nil)                      // Aucune icône
+		overlay := container.NewMax(img, infoButton) // Superpose l'image et le bouton transparent
+
+		hbox := container.NewHBox(overlay, artistLabel)
+		content.Add(hbox)
+	}
+
+	scroll := container.NewScroll(content)
+	w.SetContent(scroll)
+	w.Resize(fyne.NewSize(800, 600))
+	w.ShowAndRun()
 }
